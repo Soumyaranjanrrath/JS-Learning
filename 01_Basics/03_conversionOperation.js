90ff307ea460c9fa758efe6a47f45a14e1a34427
Soumyaranjanrrath <162270796+Soumyaranjanrrath@users.noreply.github.com>
2025-06-22 15:29:42 +0000

    conversion of datatypes

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+let score =  true
+ 
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+/*
+"33" => 33
+"33ffd" => NaN
+null => 0
+true => 1, false => 0
+*/
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+/*
+1 => true, 0 => false
+"" => false
+"tapan" => true
+*/
+
+let someNumber = 33
+let StrNumber = String(someNumber)
+console.log(StrNumber);
+console.log(typeof(StrNumber));
