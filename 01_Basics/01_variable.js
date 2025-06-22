d0b74c78739aaaecdd7819bf2fb7ddb1028fdd58
Soumyaranjanrrath <162270796+Soumyaranjanrrath@users.noreply.github.com>
2025-06-22 15:14:38 +0000

    variables & dataTypes

---
+++ 01_Basics/01_variable.js
@@ -0,0 +1,17 @@
+const accountId = 144552
+let acccEmail = "tapanrath2005@gmail.com"
+var accPass ="12334"
+accCity= "Gunupur"
+
+// accountId =2
+
+acccEmail ="dde@ff.com"
+accPass = "22333"
+
+console.table([accountId,acccEmail,accPass,accCity])
+console.log(accountId);
+
+/* 
+Prefer not to use var, because of issue in block and functional space.
+
+*/ 
---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS code as newer version
+
+//alert( 3 + 3 ) // we are using node.js not browser
+
+console.log(3+3); 
+
+// DATATYPES
+
+// number=> 2 to the power 52
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value not assigned
+// symbol => used for unique
+
+
+// object
+console.log(typeof "tapan") //string
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
+
