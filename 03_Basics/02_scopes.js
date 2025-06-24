884f718c72e837165befcba283fed71ed91ca66c
Soumyaranjanrrath <162270796+Soumyaranjanrrath@users.noreply.github.com>
2025-06-24 18:13:21 +0000

    scopes

---
+++ 03_Basics/02_scopes.js
@@ -0,0 +1,34 @@
+//var c = 30;
+// let  a = 300
+// if(true){
+//     let a = 10 
+//     const b = 20
+//     console.log("INNER SCOPE : ", a); // 10
+// }
+
+
+
+// console.log(a); // 10
+// console.log(b); // 20       
+// console.log(c); // 30
+
+
+function one() {
+    const userName = "Tapan";
+
+    function two() {
+        const age = 20;
+        console.log(userName);
+    }
+    console.log(age); // This will throw an error because 'age' is not defined in this scope
+    two(); // Calling the inner function to display the username
+}
+// one(); // Calling the outer function to execute the code
+
+if (true) {
+    const userName = "Tapan";
+    if ( userName === "Tapan") {
+        const age = 20;
+        console.log(userName); 
+        console.log(age); // This will work because 'age' is defined in this scope
+        }
