e025f5d23f52b33d9bfc61dadf1dfd37318e9c06
Soumyaranjanrrath <162270796+Soumyaranjanrrath@users.noreply.github.com>
2025-06-27 15:45:32 +0000

    this and arrow

--- 03_Basics/03_arrow.js
+++ 03_Basics/03_arrow.js
@@ -13,5 +13,35 @@ const user = {
 // user.name = "Tapan";
 // user.welcomeMessage(); // Tapan is welcome to the course
 
-console.log(this);
+// console.log(this);
 
+// function one() {
+//   let user="tapan";
+//   console.log(this.user); // undefined in strict mode, global object in non-strict mode  
+// }
+
+// const one = function() {
+//   let user = "tapan";
+//   console.log(this.user); // undefined in strict mode, global object in non-strict mode  
+// }
+
+
+// const one = () => {
+//   let user = "tapan";
+//   console.log(this.user); // undefined in strict mode, global object in non-strict mode
+// }
+
+// one(); 
+
+// const addTwo = (n1,n2) => {
+//   return n1 + n2;
+// }
+
+//or
+// const addTwo = (n1,n2) => n1 + n2;
+//or
+// const addTwo = (n1,n2) => (n1 + n2);
+
+const addTwo = (n1,n2) => ({user : "Tapan"})
+console.log(addTwo(10,20)); // { user: 'Tapan' }
+// console.log(addTwo(10,20)); // 30
